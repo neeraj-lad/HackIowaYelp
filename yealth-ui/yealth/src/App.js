290e0cc91800205a06ce0f15ec488f3cc5a06c19
Ryan Fehr <rfehr@umail.iu.edu>
2017-09-17 12:12:58 -0400

    Working heatmap of unemployment

--- yealth-ui/yealth/src/App.js
+++ yealth-ui/yealth/src/App.js
@@ -6,16 +6,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        {/* <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
 }
 
 export default App;
+
+// import BarChart from './BarChart'
+// class App extends Component {
+//    render() {
+//    return (
+//       <div className='App'>
+//       <div className='App-header'>
+//       <h2>d3ia dashboard</h2>
+//       </div>
+//       <div>
+//       <BarChart data={[5,10,1,3]} size={[500,500]} />
+//       </div>
+//       </div>
+//    )
+//    }
+// }
+// export default App
